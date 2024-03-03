3d47d3068493dab8d0fe36209701cc52d205017b
Rogelio Menco <rogeliomenco4@gmail.com>
2024-03-03 18:06:52 -0500

    chore: update to hash router for gh-pages

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,18 @@
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './Store/index';
 import './index.css';
 
-const basePath = process.env.PUBLIC_URL ?? '/';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+// Para Github Pages necesitamos HashRouter de lo contrario podemos usar BrowserRouter
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename={basePath}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
 );
 
